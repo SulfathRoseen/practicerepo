140fb7ab534171ea9d7f1bf7e4dbcb54470cd446
Sulfath Roseen <Sulfath Roseen@LAPTOP-VT3SL9S3>
2024-12-31 21:39:21 +0530

    push

---
+++ mypackage/src/main/java/mypackage2/Array.java
@@ -0,0 +1,20 @@
+package mypackage2;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[3];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		int i;
+		for(i=0; i<=2; i++)
+		{
+		System.out.println(a[i]);
+		}
+		}
+
+	}
+
+
